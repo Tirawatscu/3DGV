571649bf04f42ee5771dd63124425fc9819fb6d5
tirawatscu <tirawat.scu97@gmail.com>
2023-09-10 15:34:04 +0700

    first commit

---
+++ Server.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, json, request
+from flask_socketio import SocketIO
+import paho.mqtt.client as mqtt
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+known_clients = [
+    {'client_id': 'device1', 'status': 'disconnected', 'rssi': None, 'last_updated': None},
+    {'client_id': 'device2', 'status': 'disconnected', 'rssi': None, 'last_updated': None},
+    {'client_id': 'device3', 'status': 'disconnected', 'rssi': None, 'last_updated': None},
+]
+
+def custom_json(obj):
+    if isinstance(obj, datetime):
+        return obj.strftime('%Y-%m-%d %H:%M:%S')
+    raise TypeError('Type not serializable')
+
+@socketio.on('connect')
+def handle_connect(auth=None):
+    socketio.emit('update_clients', json.dumps(known_clients, default=custom_json))
+    print(f"Emitting update at {datetime.now().strftime('%H:%M:%S')}")
+
+def check_client_status():
+    while True:
+        socketio.sleep(10)
+        for client in known_clients:
+            if client['last_updated'] is None:
+                continue
+            if datetime.now() - client['last_updated'] > timedelta(seconds=20):
+                client['status'] = 'disconnected'
+                client['rssi'] = None
+        socketio.emit('update_clients', json.dumps(known_clients, default=custom_json))
+
+def on_connect(mqtt_client, userdata, flags, rc):
+    print(f'Connected with result code {rc}')
+    for client in known_clients:
+        mqtt_client.subscribe(f"{client['client_id']}/rssi")
+        mqtt_client.subscribe(f"{client['client_id']}/data")
+
+def on_message(mqtt_client, userdata, msg):
+    print(f'{msg.topic} {str(msg.payload.decode())}')
+    client_id, data_type = msg.topic.split('/')
+    data = msg.payload.decode()
+    for client in known_clients:
+        if client['client_id'] == client_id:
+            if data_type == 'rssi':
+                client['status'] = 'connected'
+                client['rssi'] = data
+                client['last_updated'] = datetime.now()
+            elif data_type == 'data':
+                socketio.emit('update_data', json.dumps({'client_id': client_id, 'data': data}))
+
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.connect('192.168.1.52', 1883, 60)
+mqtt_client.loop_start()
+
+@socketio.on('start_collecting')
+def handle_start_collecting(data):
+    start_time = datetime.now() + timedelta(seconds=5)
+    duration = data.get('duration', 30)
+    for client in known_clients:
+        if client['status'] == 'connected':
+            command = {
+                "start_time": start_time.strftime('%Y-%m-%d %H:%M:%S'),
+                "duration": duration
+            }
+            mqtt_client.publish(f"{client['client_id']}/command", json.dumps(command))
+
+@app.route('/')
+def index():
+    return render_template('index.html', clients=known_clients)
+
+if __name__ == '__main__':
+    socketio.start_background_task(check_client_status)
+    socketio.run(app, host='0.0.0.0', port=5000)
---
+++ clientv001.py
@@ -0,0 +1,82 @@
+import paho.mqtt.client as mqtt
+import time
+import json
+import numpy as np
+import datetime  # Import the datetime library
+import subprocess
+import platform
+import threading  # Import the threading library
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe(f"{client_id}/command")
+
+def get_real_rssi():
+    try:
+        os_platform = platform.system()
+        command_output = ''
+        if os_platform == 'Linux':
+            command_output = subprocess.check_output(['iwconfig'], stderr=subprocess.STDOUT, text=True, universal_newlines=True)
+            lines = command_output.split('\n')
+            for line in lines:
+                if 'Link Quality' in line:
+                    rssi = line.split('=')[1].split('/')[0].strip()
+                    return int(rssi)
+        elif os_platform == 'Darwin':
+            command_output = subprocess.check_output(
+                ['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I'],
+                stderr=subprocess.STDOUT,
+                text=True,
+                universal_newlines=True
+            )
+            lines = command_output.split('\n')
+            for line in lines:
+                if 'agrCtlRSSI' in line:
+                    rssi = line.split(':')[1].strip()
+                    return int(rssi)
+        else:
+            print(f'Unsupported platform: {os_platform}')
+            return 0
+    except Exception as e:
+        print(f'An error occurred: {e}')
+        return 0
+
+def collect_data(client, userdata, msg):
+    print(f"Received command on topic {msg.topic}: {msg.payload.decode()}")
+    command = json.loads(msg.payload.decode())
+    start_time = datetime.datetime.strptime(command['start_time'], '%Y-%m-%d %H:%M:%S')
+    duration = int(command['duration'])
+    freq = 1  # 1 Hz
+    sampling_rate = 100  # 100 Hz
+    values = []
+    for t in np.arange(0, duration, 1 / sampling_rate):
+        sine_wave = np.sin(2 * np.pi * freq * t)
+        values.append(sine_wave)
+        time.sleep(1 / sampling_rate)
+    mqtt_client.publish(f"{client_id}/data", json.dumps(values))
+    print(f"Sent data to the server")
+
+def on_command(client, userdata, msg):
+    data_thread = threading.Thread(target=collect_data, args=(client, userdata, msg))
+    data_thread.start()
+
+def publish_rssi():
+    while True:
+        rssi_value = get_real_rssi()
+        if rssi_value is not None:
+            print(f"Publishing RSSI value: {rssi_value}")
+            mqtt_client.publish(f"{client_id}/rssi", rssi_value)
+        time.sleep(5)
+
+client_id = "device1"
+
+mqtt_client = mqtt.Client(client_id=client_id)
+mqtt_client.on_connect = on_connect
+mqtt_client.message_callback_add(f"{client_id}/command", on_command)
+
+mqtt_client.connect("192.168.1.52", 1883, 60)
+
+mqtt_client.loop_start()
+
+rssi_thread = threading.Thread(target=publish_rssi)
+rssi_thread.start()
