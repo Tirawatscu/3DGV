f93734ec8fd3db678f3aef25894945442eddd29b
tirawatscu <tirawat.scu97@gmail.com>
2023-09-13 15:34:20 +0700

    config.ini

--- Server.py
+++ Server.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 import time
 import os
 from models import db, AdcData
+import configparser  
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 Storage_path = os.path.join(base_dir, 'storage')
@@ -134,10 +135,12 @@ def index():
     return render_template('index.html', clients=known_clients)
 
 if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('config.ini')
     mqtt_client = mqtt.Client()
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
-    mqtt_client.connect('192.168.1.112', 1883, 60)
+    mqtt_client.connect(config['MQTT']['IP'], config['MQTT']['Port'], 60)
     mqtt_client.loop_start()
     
     socketio.start_background_task(check_client_status)
